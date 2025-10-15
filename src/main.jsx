c406072906c5bd6e0e27d5b18595a76679c06119
Toby Jones <toby1408@gmail.com>
2025-10-15 15:32:41 +0100

    Added initial setup of auth0 - is not using auth yet

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
@@ -7,6 +8,14 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Auth0Provider
+      domain="dev-isdsnzl58pgmx3kw.us.auth0.com"
+      clientId="5GAiTgREXXzepDRVk7QfRXr4ffPZ5r25"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
     <App />
+    </Auth0Provider>
   </StrictMode>,
 )
